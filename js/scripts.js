8b481397f02b9a4ea2c3dc5c4e96d4902ff15e4d
david mark <markkariuki17@gmail.com>
2017-11-07 14:04:57 +0300

    add change

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,26 @@ $(document).ready(function() {
 		$("form #submit").click(function() {
 
   	var email= $(".Email").val();
-    alert(email+" "+"has been successfully added to our list of subscribers.Thank you!");
+    alert(email+" "+"has been successfully added to our list of subscribers.Thank you");
   });
+});
+
+
+
+$(function() {
+    //----- OPEN
+    $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+
+        e.preventDefault();
+    });
+
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+
+        e.preventDefault();
+    });
 });
